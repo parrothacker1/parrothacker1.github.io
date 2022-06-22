2840b8d70b7391f8f449b17fbf83c4c5d66e0951
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2022-06-22 05:04:34 +0000

    React website revised

--- src/App.js
+++ src/App.js
@@ -1,80 +1,96 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from "react";
 import profile from './profile.jpg';
-import profile2 from "./profile2.jpg";
 import './css/App.css';
-import { BsInstagram,BsArrowDown } from "react-icons/bs";
-import { FiHeart,FiGithub,FiTwitter } from "react-icons/fi";
-import { MdWorkOutline } from "react-icons/md";
-import { FaTelegramPlane } from "react-icons/fa";
-import { ImReddit } from "react-icons/im";
-import { Container,Row,Col,ListGroup } from "react-bootstrap";
+import { BsInstagram as Instagram,BsCodeSlash as Code } from "react-icons/bs";
+import { FiGithub as GitHub,FiTwitter as Twitter } from "react-icons/fi";
+import { FaTelegramPlane as Telegram,FaAndroid as Android } from "react-icons/fa";
+import { ImReddit as Reddit } from "react-icons/im";
+import { TbBrandDiscord as Discord } from "react-icons/tb";
+import { RiMusicLine as Music,RiComputerLine as Computer } from "react-icons/ri";
+import { VscUnlock as Unlock,VscGlobe as Globe,VscTerminalLinux as Linux } from "react-icons/vsc";
+import { Container,Row,Col } from "react-bootstrap";
 
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-      <Container>
-        <Container fluid className="first text-center d-flex flex-wrap align-items-center">
-      <Container classNmae="text-center justify-content-center d-flex flex-wrap align-items-center">
-            <font size="40" className="intro-text">Hello,I am parrothacker1</font>
-      </Container>
-      <Container fluid className="text-center justify-content-center d-flex flex-wrap align-items-center">
-            <Row className="text-center d-flex flex-wrap justify-content-center">
-                <Col className="text-center d-flex flex-wrap justify-content-center"><img src={profile} className="logo" alt="logo"/></Col>
-                <Col className="text-center d-flex flex-wrap justify-content-center"><img src={profile2} className="logo"alt="logo"/></Col>
-            </Row>
-      </Container>
-            <Container fluid className="text-center">
-                <Row>
-                    <Col className="text-center">
-                        <BsInstagram onClick={() => window.open("https://www.instagram.com/parrothacker1","_self")} size="30"/>
-                    </Col>
-                    <Col className="text-center">
-                        <FiGithub onClick={() => window.open("https://www.github.com/parrothacker1","_self")} size="30"/>
-                    </Col>
-                    <Col className="text-center">
-                        <FaTelegramPlane onClick={() => window.open("https://t.me/parrothacker1","_self")} size="30"/>
-                    </Col>
-                    <Col className="text-center">
-                        <FiTwitter size="30" onClick={() => window.open("https://twitter.com/parrothacker1","_self")} />
-                    </Col>
-                    <Col className="text-center">
-                        <ImReddit onClick={() => window.open("https://www.reddit.com/u/parrothacker1","_self")} size="30"/>
-                    </Col>
-            </Row>
-            </Container>
-            <Container fluid className="text-center"><BsArrowDown onClick={() => window.open("#about","_self")} size="30"/></Container>
-            </Container>
-        </Container>
-        <br></br>
-        <br></br>
-        <Container fluid className="parent_info">
-            <br></br>
-            <br></br>
-            <Container fluid className="intro" id="about">
-        I am a 17 year old fella who likes Ethical Hacking and Full Stack Development.I am currently pursuing my 12th education in CBSE. I have been working on my skills for the past 2 years with the help of ... ofc Google,Stackoverflow,Javapoint,etc. I am eagerly waiting to crack OSCP and get certified as a Ethical Hacker and to be a part-time Full-Stack Developer. I am also interested in maintaining and creating github projects. Always follows KISS principle (Keep It Simple Stupid).
-        </Container>
-        <br></br><br></br>
-        <Container fluid className="text-center">
-            <ListGroup className="list justify-content-center align-items-center text-center">
-                <ListGroup.Item className="list-ele border-0"><MdWorkOutline /> Full Stack Development</ListGroup.Item>
-                <ListGroup.Item className="list-ele border-0"><FiHeart /> Ethical Hacking</ListGroup.Item>
-                <ListGroup.Item className="list-ele border-0"><FiHeart /> Linux Enthusiast</ListGroup.Item>
-                <ListGroup.Item className="list-ele border-0"><FiHeart /> Arch Linux</ListGroup.Item>
-                <ListGroup.Item className="list-ele border-0"><FiHeart /> GitHub Coder</ListGroup.Item>
-            </ListGroup>
-        </Container>
-        <br></br>
-        <Row>
-            <Col className="text-center cpy">
-                 &copy; Copyrights All Reserved
+<div>
+    <header className="App-header">
+        <Container fluid className="first">
+          <Row className='top-bar'>
+            <Col className='d-flex flex-wrap justify-content-start align-items-center'>
+              <img src={profile} alt="small_logo" className="logo" />
             </Col>
-         </Row>
-    </Container>
+            <Col className="d-flex flex-wrap justify-content-end align-items-center">
+              <Instagram className='icons' onClick={() => window.open("https://www.instagram.com/parrothacker1","_self")} />
+              <Twitter className='icons' onClick={() => window.open("https://twitter.com/parrothacker1","_self")} />
+              <GitHub className='icons' onClick={() => window.open("https://www.github.com/parrothacker1","_self")} />
+              <Telegram className='icons' onClick={() => window.open("https://t.me/parrothacker1","_self")} />
+              <Reddit className='icons' onClick={() => window.open("https://www.reddit.com/u/parrothacker1","_self")} />
+              <Discord className='icons' onClick={() => window.open("https://discordapp.com/users/806844002271494144","_self")}/>
+            </Col>
+          </Row>
+          <Row className='intro'>
+            <Col fluid className="text d-flex justify-content-center align-items-center">
+
+              <font>Hello Strangers there.I'm <b>parrothacker1</b>.I'm a <b>17 year old</b> fella from <b>India</b> who is interested in cybersec and dev stuff.</font>
+
+            </Col>
+            <Col fluid className="d-flex flex-wrap justify-content-center align-items-center">
+              <img src={profile} alt="logo" className="logo" />
+            </Col>
+          </Row>
+        </Container>
+        <Container fluid className="d-flex flex-wrap justify-content-center align-items-center">
+          <Container fluid className="card d-flex flex-wrap justify-content-center align-items-center">
+            <Container fluid className="card-text">
+              <b>About</b><br></br><br></br>
+              I'm a 17 year old student from India and I am currently pursuing my education. I'm interested in website development
+              and android app development. Also I'm a cybersecurity enthusiast. I love to spend time on learning about linux too.I 
+              use NixOS as my main distribution and i3 as my window manager. My aim is to complete OSCP and get certified as a 
+              ethical hacker.
+            </Container>
+          </Container>
+          <Container fluid className="card d-flex flex-wrap justify-content-center align-items-center">
+            <Container fluid className="card-text">
+              <b>Skills</b><br></br><br></br>
+              <Container fluid>
+                <Container fluid className="interest-card d-flex flex-wrap align-items-center"><Globe />&emsp;Web development</Container>
+                <Container fluid className="interest-card d-flex flex-wrap align-items-center"><Android />&emsp;App development</Container>
+                <Container fluid className="interest-card d-flex flex-wrap align-items-center"><Linux />&emsp;Linux</Container>
+                <Container fluid className="interest-card d-flex flex-wrap align-items-center"><Unlock />&emsp;Pentesting</Container>
+              </Container>
+            </Container>
+          </Container>
+          <Container fluid className="card d-flex flex-wrap justify-content-center align-items-center">
+            <Container fluid className='card-text'>
+              <b>Interests</b><br></br><br></br>
+              <Container fluid>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card"><Code />&emsp;Developing Projects</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card"><Computer />&emsp;Playing CTFs</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card"><Music />&emsp;Listening to Music</Container>
+              </Container>
+            </Container>
+          </Container>
+          <Container fluid className="card d-flex flex-wrap justify-content-center align-items-center">
+            <Container fluid className='card-text'>
+              <b>Social Platforms</b><br></br><br></br>
+              <Container fluid>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://www.instagram.com/parrothacker1","_self")}><Instagram />&emsp;Instagram</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://twitter.com/parrothacker1","_self")}><Twitter />&emsp;Twitter</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://www.github.com/parrothacker1","_self")}><GitHub />&emsp;GitHub</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://t.me/parrothacker1","_self")}><Telegram />&emsp;Telegram</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://www.reddit.com/u/parrothacker1","_self")}><Reddit />&emsp;Reddit</Container>
+                <Container fluid className="d-flex flex-wrap align-items-center interest-card" onClick={() => window.open("https://discordapp.com/users/806844002271494144","_self")}><Discord />&emsp;Discord</Container>
+              </Container>
+            </Container>
+          </Container>
+        </Container>
+        <footer className='d-flex flex-wrap justify-content-center align-items-center'>
+          <font>&copy; Copyrights All Reserved</font>
+        </footer>
     </header>
-    </div>
+</div>
   );
 }
 
